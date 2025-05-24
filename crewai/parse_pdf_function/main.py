4673c5410cc109e8eafa64451cd640f3b87c6931
BernardoTxr <bernardoteixeira@usp.br>
2025-05-24 14:32:57 -0300

    debugging parse api

--- crewai/parse_pdf_function/main.py
+++ crewai/parse_pdf_function/main.py
@@ -18,20 +18,34 @@ def processar_pdf():
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(filename)
 
+    print(f"Downloading {filename} from bucket {bucket_name}...")
+
     local_input_path = f"/tmp/{filename}"
     blob.download_to_filename(local_input_path)
+
+    print(f"Downloaded {filename} to {local_input_path}")
 
     converter = DocumentConverter()
     result = converter.convert(local_input_path)
 
+    print(f"Conversion done")
+
     markdown = result.document.export_to_markdown()
+
+    print(f"Markdown conversion done")
 
     output_filename = f"{filename}"
     output_blob = bucket.blob(output_filename)
 
+    print(f"Uploading {output_filename} to bucket {bucket_name}...")
+
     with open("/tmp/output.md", "w") as f:
         f.write(markdown)
 
+    print(f"Markdown written to /tmp/output.md")
+
     output_blob.upload_from_filename("/tmp/output.md")
+
+    print(f"Uploaded {output_filename} to bucket {bucket_name}")
 
     return jsonify({"status": "success", "output": output_filename})
